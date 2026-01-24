b9fef26d0cc87fdb8c12983d6933f573c5a9dd23
[dyad] <osorbas@gmail.com>
2026-01-24 12:53:10 +0000

    [dyad] Fixed syntax error in FirstTimeSetupDialog.tsx import. - wrote 1 file(s)

--- src/components/FirstTimeSetupDialog.tsx
+++ src/components/FirstTimeSetupDialog.tsx
@@ -6,7 +6,7 @@ import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, D
 import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import * => z from "zod";
+import * as z from "zod";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { usePrinters, Printer } from "@/hooks/use-printers";
 import { useFilaments, NewFilamentData } from "@/hooks/use-filaments";
